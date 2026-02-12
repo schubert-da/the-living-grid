3fccf27f973c64bb9700707c7c0141220e0709dd
schubert-da <schubertdeabreu2@gmail.com>
2026-02-12 21:48:57 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="grid-container py-20">
+    <div class="title-content flex flex-col items-center gap-4">
+        <h1 class='text-center text-3xl font-bold'>Japan Gallery grid</h1>
+        <p class='text-center max-w-[80ch] text-balance'> I hate how often we document things for the sole purpose of sharing, how everything is curated for online consumption and any other form of capture seems less than.</p>
+    </div>
+</div>
