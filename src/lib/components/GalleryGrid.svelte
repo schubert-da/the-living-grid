162884822719f0cdede22079076d697433d17339
schubert-da <schubertdeabreu2@gmail.com>
2026-02-14 19:18:18 +0530

    initial grid implementation

---
+++ src/lib/components/GalleryGrid.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let images = [];
+    const aspectRatio = 435 / 500; 
+</script>
+
+<div class="grid-container w-full flex justify-center">
+    <div class="grid max-auto max-w-250 gap-0.5">
+        {#each [...images, ...images] as image}
+            <div class="grid-item" style="aspect-ratio: {aspectRatio}">
+                <img  src={image.image} alt={image.title || image.description} class="w-full h-full object-cover" />
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .grid {
+        grid-template-columns: repeat(auto-fit, minmax(180px, 1fr));
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
-    export let data;
+	import GalleryGrid from '$lib/components/GalleryGrid.svelte';
 
+    export let data;
     console.log("data", data);
 </script>
 
-<div class="grid-container py-20">
+<div class="grid-container flex flex-col items-center gap-10">
     <div class="title-content flex flex-col items-center gap-4">
         <h1 class='text-center text-3xl font-bold'>Japan Gallery grid</h1>
         <p class='text-center max-w-[80ch] text-balance'> I hate how often we document things for the sole purpose of sharing, how everything is curated for online consumption and any other form of capture seems less than.</p>
     </div>
+
+    <GalleryGrid images={data.images} />
 </div>
