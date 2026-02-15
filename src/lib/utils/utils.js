89838bc804c2833c51b09b9c633a101f6f39bbca
schubert-da <schubertdeabreu2@gmail.com>
2026-02-15 09:44:56 +0530

    add palette colors

---
+++ src/lib/utils/utils.js
@@ -0,0 +1,32 @@
+export const palette = [
+    {
+        name: 'red',
+        color: 'var(--color-palette-red)',
+        text: '#222',
+        border: '#222'
+    },
+    {
+        name: 'blue',
+        color: 'var(--color-palette-blue)',
+        text: '#fff',
+        border: '#222'
+    },
+    {
+        name: 'yellow',
+        color: 'var(--color-palette-yellow)',
+        text: '#222',
+        border: '#222'
+    },
+    {
+        name: 'orange',
+        color: 'var(--color-palette-orange)',
+        text: '#222',
+        border: '#222'
+    },
+    {
+        name: 'cream',
+        color: 'var(--color-palette-cream)',
+        text: '#222',
+        border: '#222'
+    }
+]
