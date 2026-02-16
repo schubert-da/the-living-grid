4caf3f013fa9fac9cfa4af02b8fcc4b7d5f0ae90
schubert-da <schubertdeabreu2@gmail.com>
2026-02-16 16:15:47 +0530

    github pages setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,29 @@
 import adapter from '@sveltejs/adapter-static';
 import { mdsvex } from 'mdsvex';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: ['.svelte', '.md'],
 
     preprocess: [
         mdsvex({
-            extensions: ['.md'],
+            extensions: ['.md']
         })
     ],
 
     kit: {
-        adapter: adapter()
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+
+        paths: {
+            base: dev ? '' : '/the-living-grid',
+            assets: dev ? '' : '/the-living-grid'
+        }
     }
 };
 
