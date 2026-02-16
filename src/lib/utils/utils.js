2c4b3e39ec7f2187eeefc89326ec1c0c0ace33fc
schubert-da <schubertdeabreu2@gmail.com>
2026-02-16 10:07:57 +0530

    adds seeded randomness

--- src/lib/utils/utils.js
+++ src/lib/utils/utils.js
@@ -30,3 +30,26 @@ export const palette = [
         border: '#222'
     }
 ]
+
+function mulberry32(seed) {
+    return function () {
+        let t = seed += 0x6D2B79F5;
+        t = Math.imul(t ^ (t >>> 15), t | 1);
+        t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+        return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
+    };
+}
+
+function hashString(str) {
+    let h = 0;
+    for (let i = 0; i < str.length; i++) {
+        h = Math.imul(31, h) + str.charCodeAt(i);
+    }
+    return h >>> 0;
+}
+
+export function dailyRandom(date, key) {
+    const baseSeed = new Date(date).getTime() / (1000 * 60 * 60 * 24);
+    const seed = baseSeed + hashString(key);
+    return mulberry32(seed)();
+}
