37ea20c4efecd4b53bb70c9d9139257b4258c140
schubert-da <schubertdeabreu2@gmail.com>
2026-03-03 10:19:28 +0530

    update writeup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,9 +7,10 @@
 
 <div class="grid-container flex flex-col items-center gap-5">
     <div class="title-content flex flex-col items-center gap-4 mb-5">
-        <h1 class='text-center text-3xl sm:text-5xl font-bold'>The Grid</h1>
-        <p class='text-center text-pretty max-w-[70ch] text-sm sm:text-xl'> This is a weird experiment because I wanted to document my travel a bit more. Each day the grid will randomly spawn new layouts, image filters and arrangements, but I won't know what it will look like until I start filling out the grid. </p>
-        <p class='text-center text-pretty max-w-[70ch] text-sm sm:text-xl'>I'm also in Japan currently and have no way to fix this if it goes to hell... Enjoy!</p>
+        <h1 class='text-center text-3xl sm:text-5xl font-bold'>The Living Grid - Japan</h1>
+        <p class='text-center text-pretty max-w-[80ch] text-sm sm:text-lg leading-[1.4]'> I like to document my travels capturing odd details that I find interesting - a bit of decor, how I feel in the moment, weird things that happened. The only place I would upload these photos would be Instagram, but the over time I learnt that I was starting to taking photos <em>for Instagram rather than myself</em> - stories, photos and reels all needed to look *a certain way* which to me felt forced and unnatural. </p>
+        <p class='text-center text-pretty max-w-[80ch] text-sm sm:text-lg leading-[1.4]'>That's why this page exists - based on the date, photos are compressed, randomly layouts change and filters will be applied, leaving myself little control over how things look and instead lets me just take a <em>good enough</em> memory instead of repeatedly wasting time taking the "perfect picture" which I'd likely forget anyways. All I have to do is just fill out the grid and see how it turns out.</p>
+        <p class='text-center text-pretty max-w-[80ch] text-sm sm:text-lg leading-[1.4]'>I'm also in Japan currently and have no way to fix this if it goes to hell... Enjoy!</p>
     </div>
 
     <GalleryGrid images={data.images} />
